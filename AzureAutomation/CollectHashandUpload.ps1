befca861f956822f4078c33284e6477437c61b7a
Michael Meier <83367358+mmeierm@users.noreply.github.com>
2023-10-16 19:13:32 +0200

    Added support for a minimal GroupTag selection UI

--- AzureAutomation/CollectHashandUpload.ps1
+++ AzureAutomation/CollectHashandUpload.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+Retrieves the Windows AutoPilot deployment details from WinPE 
+
+Expects "oa3tool.exe" from the Windows ADK (default at "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Licensing\OA30") in the same folder as this script to create the HW Hash
+In order to also detect the TPM Module as needed for Self-Deploying Mode, we need the File "PCPKsp.dll" from "C:\Windows\System32" from a Full Windows Installation
+#>
+
+# base parameters
+$GroupTag="" #Define if static GroupTag needed
+$GroupTagUI = $false
+$GroupTagUIList=@()
+$GroupTagUIList =@('Tag1','Default','Other')
+$WebhookURI="<-------------INSERT WEBHOOK URL HERE--------------->"
+
 
 Function Start-Command {
     Param([Parameter (Mandatory=$true)]
@@ -23,8 +38,82 @@ Function Start-Command {
     }
 }
 
-# base parameters
-$GroupTag="" #Define if needed
+#GUI for GroupTag selection
+If ($GroupTagUI)
+{
+    write-host "Starting GroupTag selection..."
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[System.Windows.Forms.Application]::EnableVisualStyles();
+$objForm = New-Object System.Windows.Forms.Form
+$objForm.Backcolor="white"
+$objForm.Text = "GroupTag selection"
+$objForm.FormBorderStyle = 'Fixed3D'
+$objForm.MaximizeBox = $false
+
+$GroupTagCombobox=New-object system.windows.forms.combobox
+$GroupTagCombobox.Location = New-Object System.Drawing.Size(10,10)
+$GroupTagCombobox.Size = New-Object System.Drawing.Size(220,40)
+$GroupTagCombobox.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 12, [System.Drawing.FontStyle]::Bold)
+
+foreach ($Tag in $GroupTagUIList){
+    $GroupTagCombobox.Items.Add($Tag) | Out-Null
+    }
+
+$GroupTagBTN=New-Object System.Windows.Forms.Button
+$GroupTagBTN.Location = New-Object System.Drawing.Size(10,50)
+$GroupTagBTN.Size = New-Object System.Drawing.Size(220,40)
+$GroupTagBTN.Text = "Submmit"
+$GroupTagBTN.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 14, [System.Drawing.FontStyle]::Bold)
+$GroupTagBTN.DialogResult = "Cancel"
+
+
+$GroupTagBTN.add_Click({
+
+    $Script:GroupTag=$GroupTagCombobox.SelectedItem  
+})
+
+$objForm.Controls.Add($GroupTagCombobox)
+$objForm.Controls.Add($GroupTagBTN)
+$objForm.Size = New-Object System.Drawing.Size(250,130)
+
+
+[void] $objForm.ShowDialog()
+}
+
+
+#Create the ConfigFiles for OA3Tool
+
+$inputxml=@' 
+<?xml version="1.0"?>
+  <Key>
+    <ProductKey>XXXXX-XXXXX-XXXXX-XXXXX-XXXXX</ProductKey>
+    <ProductKeyID>0000000000000</ProductKeyID>  
+    <ProductKeyState>0</ProductKeyState>
+  </Key>
+'@
+
+$oa3cft=@' 
+<OA3>
+   <FileBased>
+       <InputKeyXMLFile>".\input.XML"</InputKeyXMLFile>
+   </FileBased>
+   <OutputData>
+<AssembledBinaryFile>.\OA3.bin</AssembledBinaryFile>
+<ReportedXMLFile>.\OA3.xml</ReportedXMLFile>
+   </OutputData>
+</OA3>
+'@
+
+If(!(Test-Path $PSScriptRoot\input.xml))
+{
+    New-Item "$PSScriptRoot\input.xml" -ItemType File -Value $inputxml
+}
+If(!(Test-Path $PSScriptRoot\OA3.cfg))
+{
+    New-Item "$PSScriptRoot\OA3.cfg" -ItemType File -Value $oa3cft
+}
 
 $serial = (Get-WmiObject -Class Win32_BIOS).SerialNumber
 
@@ -60,7 +149,7 @@ $body = @{
 }
 $json = $body | ConvertTo-Json
 
-$HTTPResult=Invoke-RestMethod -Method Post -Uri "<-------------INSERT WEBHOOK URL HERE--------------->" -Body $json -ContentType 'application/json'
+$HTTPResult=Invoke-RestMethod -Method Post -Uri $WebhookURI -Body $json -ContentType 'application/json'
 If ($HTTPResult.JobIds)
 {
 write-output "Started Upload with JobID "$HTTPResult.JobIds""
