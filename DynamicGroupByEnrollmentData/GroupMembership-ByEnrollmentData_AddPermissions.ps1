5c6d34e52037b82bfbd468c0093ad963d3e07fdd
Michael Meier <83367358+mmeierm@users.noreply.github.com>
2024-02-11 14:28:38 +0100

    Update GroupMembership-ByEnrollmentData_AddPermissions.ps1

--- DynamicGroupByEnrollmentData/GroupMembership-ByEnrollmentData_AddPermissions.ps1
+++ DynamicGroupByEnrollmentData/GroupMembership-ByEnrollmentData_AddPermissions.ps1
@@ -4,7 +4,7 @@ Connect-MgGraph -Scopes Application.ReadWrite.All, AppRoleAssignment.ReadWrite.A
 $PermissionName = "GroupMember.ReadWrite.All" 
 $PermissionName2 = "Device.Read.All"
 $PermissionName3 = "DeviceManagementManagedDevices.Read.All"
-$SystemmanagedIdentity = "be166005-7100-454c-855c-01c4d63f2cdd" #"<INSERT SYSTEMMANAGED IDENTITY ID HERE>"
+$SystemmanagedIdentity = "<INSERT SYSTEMMANAGED IDENTITY ID HERE>"
 
 $GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
 $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
