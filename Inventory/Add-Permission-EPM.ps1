35199240e122ce7ea05ec47ce690726d7474f0f9
Michael Meier <83367358+mmeierm@users.noreply.github.com>
2024-04-28 11:04:58 +0200

    Create Add-Permission-EPM.ps1

---
+++ Inventory/Add-Permission-EPM.ps1
@@ -0,0 +1,10 @@
+Install-Module Microsoft.Graph -Scope CurrentUser
+Connect-MgGraph -Scopes Application.ReadWrite.All, AppRoleAssignment.ReadWrite.All
+
+$PermissionName = "DeviceManagementConfiguration.Read.All" 
+$SystemmanagedIdentity = "<INSERT SYSTEMMANAGED IDENTITY ID HERE>"
+
+$GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+
+New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $SystemmanagedIdentity -PrincipalId $SystemmanagedIdentity -ResourceId $GraphServicePrincipal.Id -Id $AppRole.Id -AppRoleId $AppRole.Id
