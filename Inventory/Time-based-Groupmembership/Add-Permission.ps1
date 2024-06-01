2ef81b5178a7af6f531118a41488beaaaf759439
Michael Meier <83367358+mmeierm@users.noreply.github.com>
2024-06-01 17:19:29 +0200

    Add files via upload

---
+++ Inventory/Time-based-Groupmembership/Add-Permission.ps1
@@ -0,0 +1,14 @@
+Install-Module Microsoft.Graph -Scope CurrentUser
+Connect-MgGraph -Scopes Application.ReadWrite.All, AppRoleAssignment.ReadWrite.All
+
+$PermissionName = "DeviceManagementManagedDevices.Read.All" 
+$PermissionName2 = "User.Read.All"
+$SystemmanagedIdentity = "<INSERT SYSTEMMANAGED IDENTITY ID HERE>"
+
+$GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+$AppRole2 = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName2 -and $_.AllowedMemberTypes -contains "Application"}
+
+
+New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $SystemmanagedIdentity -PrincipalId $SystemmanagedIdentity -ResourceId $GraphServicePrincipal.Id -Id $AppRole.Id -AppRoleId $AppRole.Id
+New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $SystemmanagedIdentity -PrincipalId $SystemmanagedIdentity -ResourceId $GraphServicePrincipal.Id -Id $AppRole2.Id -AppRoleId $AppRole2.Id
