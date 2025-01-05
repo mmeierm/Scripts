35c7b7dd7d96e064f8485e8cf35dddbacb805f85
Michael Meier <83367358+mmeierm@users.noreply.github.com>
2025-01-05 16:59:48 +0100

    Create RemoteFX USB Registry_remediation.ps1

---
+++ RemoteFX/RemoteFX USB Registry_remediation.ps1
@@ -0,0 +1,31 @@
+
+# Set registry key for fUsbRedirectionEnableMode
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows NT\Terminal Services\Client" -Name "fUsbRedirectionEnableMode" -Value 2 -Type DWord
+
+# Set registry key for UpperFilters
+If ((Get-ItemProperty "HKLM:\System\CurrentControlSet\Control\Class\{36fc9e60-c465-11cf-8056-444553540000}\").UpperFilters)
+{
+    If ((Get-ItemProperty "HKLM:\System\CurrentControlSet\Control\Class\{36fc9e60-c465-11cf-8056-444553540000}\").UpperFilters -notcontains "TsUSBFlt")
+    {
+
+        # Writing a REG_MULTI_SZ value
+        $upperFilters = (Get-ItemProperty "HKLM:\System\CurrentControlSet\Control\Class\{36fc9e60-c465-11cf-8056-444553540000}\").UpperFilters
+        $upperFilters = $upperFilters + "TsUSBFlt"
+
+        Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Class\{36fc9e60-c465-11cf-8056-444553540000}" -Name "UpperFilters" -Value $upperFilters -Type MultiString
+    }
+}
+else
+{
+
+    # Writing a REG_MULTI_SZ value
+    $upperFilters = @('TsUSBFlt')
+    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Class\{36fc9e60-c465-11cf-8056-444553540000}" -Name "UpperFilters" -Value $upperFilters -Type MultiString
+}
+
+# Set registry key for BootFlags
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\TsUsbFlt" -Name "BootFlags" -Value 4 -Type DWord
+
+# Set registry key for EnableDiagnosticMode
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\usbhub\hubg" -Name "EnableDiagnosticMode" -Value 0x80000000 -Type DWord
+exit 0
